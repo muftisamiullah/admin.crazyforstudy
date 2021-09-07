05f2d6f09f64bc7a7adbddf1beba173f030bc45c
Sami Ullah <muftisami2010gmail.com>
2021-09-07 20:17:59 +0530

    fixed some issues

--- controllers/student/StudentAuthController.js
+++ controllers/student/StudentAuthController.js
@@ -287,12 +287,6 @@ const changePassword = async(req,res) => {
 
 const getUser = async(req,res) => {
     try{
-        if(!req.body.email){
-            return res.status(500).json({
-                message: "Something Went Wrong"
-            });
-        }
-        console.log(req.body.email)
         let user = await Student.findOne({Email : req.body.email});
         return res.status(200).json(user);        
     }catch(e){
